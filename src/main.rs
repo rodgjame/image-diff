3f9e6485e0fadccb8a604530f5f2cf578f884bb7
James Rodgers <rodgjame@gmail.com>
2022-02-10 16:27:54 -0800

    Adding better logic to compare pixles

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,8 @@
 extern crate image;
 
 use clap::Parser;
-use image::{GenericImageView, ImageFormat, RgbImage, Rgb};
+use image::{GenericImageView, ImageFormat, RgbaImage, Rgba, Pixel};
+use std::cmp::{min, max};
 
 /// Detect the difference between 2 images
 #[derive(Parser)]
@@ -43,29 +44,44 @@ fn main() -> Result<(), Box<dyn std::error::Error>>{
 
     let first_image_width = first_image.width();
     let first_image_height = first_image.height();
-    let mut diff_image = RgbImage::new(first_image_width, first_image_height);
+    let mut diff_image = RgbaImage::new(first_image_width, first_image_height);
     let mut x = 0;
     let mut y = 0;
     let mut different_pixles = 0;
     while x < first_image_width {
         while y < first_image_height {
-            //println!("{}-{}", x, y);
-            //diff_image.put_pixel(x, y, Rgb([255, 0, 255]));
-            if (first_image.as_rgb8().unwrap().get_pixel(x, y) != second_image.as_rgb8().unwrap().get_pixel(x, y)) {
-                println!("{:?} -- {:?}", first_image.as_rgb8().unwrap().get_pixel(x, y), second_image.as_rgb8().unwrap().get_pixel(x, y));
-                different_pixles += 1;
-                diff_image.put_pixel(x, y, Rgb([255, 0, 255]));
+            let should_mark = compare_pixles(&first_image.get_pixel(x, y), &second_image.get_pixel(x, y));
+            if should_mark == true {
+                diff_image.put_pixel(x, y, Rgba([255, 0, 255, 255]));
             }
             y += 1;
         }
+
         y = 0;
         x += 1;
     }
-
-    println!("The number of different pixles is {}", different_pixles);
+    
     println!("Saving diff image to {:?}", &args.results_image);
     diff_image.save(&args.results_image).unwrap();
     println!("diff image saved");
     
     Ok(())
+}
+
+fn compare_pixles(pix_1: &image::Rgba<u8>, pix_2: &image::Rgba<u8>) -> bool {
+    let mut should_mark = false;
+    for i in 0..4 {
+        should_mark = compare_single_channel(pix_1[i], pix_2[i]);
+        if should_mark == true {
+            break;
+        }
+    }
+    return should_mark;
+}
+
+fn compare_single_channel(channel_1: u8, channel_2: u8) -> bool {
+    let number_diff = (channel_1 as i16 - channel_2 as i16) as i16;
+    let avg_of_channels = (channel_1 as i16 + channel_2 as i16) as f32 / 2 as f32;
+    let percent_diff = (number_diff.abs() as f32 / avg_of_channels) * 100 as f32;
+    return percent_diff >= 45 as f32;
 }
